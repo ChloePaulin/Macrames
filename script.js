550ae74a5356b0ab38e8da4234e4b2fd66ac49fd
chloe974 <chloe.previl@gmail.com>
2025-05-28 16:07:08 +0200

    Création du formulaire de contact

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const changeNameButton = document.getElementById("personalization");
 
 const anonymousName = document.getElementById("name_person");
-console.log(anonymousName);
 
 changeNameButton.addEventListener ("click",(changeName)=>{
         anonymousName.textContent = prompt("Saisissez votre prénom :");
